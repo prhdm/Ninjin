bfa3479cbe31a7bbd6ac541f633779570f3b8a91
Sara <sara@carriot.ir>
2022-05-12 13:49:53 +0430

    add slidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Slidebar from "../components/Slidebar";
 
 const Home = () =>{
     return (
         <>
-        <Navbar />  
+        {/* <Navbar />   */}
+        <Slidebar />
         </>
     );
 }
