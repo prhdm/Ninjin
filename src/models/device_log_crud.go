710ba8f7494ac1dde7310c81ef0d7b48d3de1fd3
ashkanzarkhah <ashkanzarkhah@Ashkans-MacBook-Air.local>
2022-05-17 18:08:58 +0500

    debuf

--- src/models/device_log_crud.go
+++ src/models/device_log_crud.go
@@ -1,13 +1,13 @@
 package models
 
-import(
+import (
 	"time"
 )
 
 func GetDeviceLog(DeviceSerial string, BeginTime time.Time, EndTime time.Time) ([]*DeviceLog, error){
 	var deviceLogSlice []*DeviceLog
 
-	result := PostgresDBProvider.DB.Where("device_serial = ? AND datetime >= ? AND datetime <= ?", DeviceSerial, BeginTime, EndTime).Find(&deviceLogSlice)
+	result := PostgresDBProvider.DB.Where("device_serial = ? AND server_time >= ? AND server_time <= ?", DeviceSerial, BeginTime, EndTime).Find(&deviceLogSlice)
 
 	return deviceLogSlice, result.Error
 }
