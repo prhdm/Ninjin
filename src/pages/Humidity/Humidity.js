2895ef013ccf0520e11a65d158836f5d69d45f9c
Sara <sara@carriot.ir>
2022-05-18 01:43:24 +0430

    water form

--- src/pages/Humidity/Humidity.js
+++ src/pages/Humidity/Humidity.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import NavbarComp from "../../components/Navbar/NavbarComp";
 import { Form,InputGroup, FormControl, Button} from 'react-bootstrap';
 import "./humidity.css";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import axios from "axios";
+import useToken from "../../components/App/useToken";
 
 const Humidity = () => {
+    const token = useToken().token;
+
+    const [deviceHumidity, setDeviceHumidity] = useState({
+        device_serial: "",
+        min_humidity: "",
+        max_humidity: "",
+    });
+    const url_setHumidity = "http://usagi.carriot.ir:8000/device/set_humidity";
+
+
+    const setHumidity = () =>{
+        console.log(token, deviceHumidity)
+        const deviceData = JSON.stringify({
+            "device_serial" : deviceHumidity.device_serial,
+            "min_humidity" : deviceHumidity.min_humidity,
+            "max_humidity" : deviceHumidity.max_humidity
+        });
+        console.log(deviceData)
+        axios.post(url_setHumidity, deviceData , {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then((response) => {
+                window.location.reload(true)
+
+            });
+    }
+
     return (
         <>
             <NavbarComp />
@@ -14,39 +45,26 @@ const Humidity = () => {
                 تنظیم رطوبت
             </div>
             <InputGroup className="mb-3 humidity-input" size="lg">
-                <FormControl aria-label="First name" placeholder="نام دستگاه" />
+                <FormControl aria-label="First name"
+                             placeholder="سریال دستگاه"
+                             onChange={(e) => setHumidity({...deviceHumidity, device_serial: e.target.value})}
+                             type="text"
+                />
             </InputGroup>
             <InputGroup dir="rtl" className="mb-3 humidity-input" size="lg">
-                {/* <InputGroup.Text>تنظیم رطوبت</InputGroup.Text> */}
-                <FormControl aria-label="min humidity" placeholder="حداقل رطوبت" />
-                <FormControl aria-label="max humidity" placeholder="حداکثر رطوبت" />
+                <FormControl aria-label="min humidity" placeholder="حداقل رطوبت"
+                             onChange={(e) => setHumidity({...deviceHumidity, min_humidity: e.target.value})}
+                             type="text" />
+                <FormControl aria-label="max humidity" placeholder="حداکثر رطوبت"
+                             onChange={(e) => setHumidity({...deviceHumidity, max_humidity: e.target.value})}
+                             type="text" />/>
 
-                    <Button variant="outline-secondary" id="button-addon2" className="add-device-btn">
+                    <Button variant="outline-secondary" id="button-addon2" className="add-device-btn"
+                    onClick={() => setHumidity()}>
                     تنظیم
                     </Button>
             </InputGroup>
-            <InputGroup dir="rtl" className="mb-3 humidity-input" size="lg">
-            <Form type="date" className="date-form">
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>تاریخ</Form.Label>
-                    <Form.Control type="date" placeholder="تاریخ" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>ساعت</Form.Label>
-                    <Form.Control type="time" placeholder="ساعت" />
-                </Form.Group>
-            </Form>
-            <Form type="date" className="date-form">
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>تاریخ</Form.Label>
-                    <Form.Control type="date" placeholder="تاریخ" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>ساعت</Form.Label>
-                    <Form.Control type="time" placeholder="ساعت" />
-                </Form.Group>
-            </Form>
-            </InputGroup>
+           
         </>
     );
 }
