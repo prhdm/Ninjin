670aceabae11969d69b601faf1d50706684e7833
ashkanzarkhah <ashkanzarkhah@Ashkans-MacBook-Air.local>
2022-05-17 17:45:28 +0500

    fixing device log

--- src/models/device_log_crud.go
+++ src/models/device_log_crud.go
@@ -7,7 +7,7 @@ import(
 func GetDeviceLog(DeviceSerial string, BeginTime time.Time, EndTime time.Time) ([]*DeviceLog, error){
 	var deviceLogSlice []*DeviceLog
 
-	result := PostgresDBProvider.DB.Where("device_serial = ? AND datetime >= ? AND datetime <= ?", DeviceSerial, BeginTime, EndTime).Find(&deviceLogSlice)
+	result := PostgresDBProvider.DB.Where("device_serial = ? AND server_time >= ? AND server_time <= ?", DeviceSerial, BeginTime, EndTime).Find(&deviceLogSlice)
 
 	return deviceLogSlice, result.Error
 }
