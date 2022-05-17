47d4043b2b27275f2d2ca5cb2bdacc775fcb64df
ashkanzarkhah <ashkanzarkhah@Ashkans-MacBook-Air.local>
2022-05-18 01:20:30 +0500

    Report table & watering log

--- src/pages/Report/Report.js
+++ src/pages/Report/Report.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, PureComponent } from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Form,InputGroup, FormControl, Button} from 'react-bootstrap';
 
 import NavbarComp from "../../components/Navbar/NavbarComp";
-import { ListGroup, InputGroup, FormControl, Button} from 'react-bootstrap';
 import "./report.css";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import axios from 'axios';
@@ -10,34 +10,69 @@ import useToken from "../../components/App/useToken";
 
 const Report = () => {
     const url_getDeviceLog = 'http://usagi.carriot.ir:8000/device/get_log';
-    //const url_getWateringLog = "http://usagi.carriot.ir:8000/device/delete_device";
-    const [devicelogs, setdevicelog] = useState([])
-    const [deviceSerial, setdeviceserial] = useState({
-        device_serial: "869170034680885",
-        begin_time: "2022-05-17T13:25:54+02:00",
-        end_time: "2022-05-17T13:26:54+02:00",
-    })
+    const url_getWateringLog = "http://usagi.carriot.ir:8000/watering-log/get";
+    const [deviceLogs, setDeviceLog] = useState([])
+    const [wateringLogs, setWateringLog] = useState([])
+    const [inputBeginDate, setBeginDate] = useState('')
+    const [inputEndDate, setEndDate] = useState('')
+    const [inputSerial, setSerial] = useState('')
+    const [currentDeviceSerial, setCurrentDeviceSerial] = useState(JSON.stringify({
+        "device_serial": "869170034680885",
+        "begin_time": "2022-05-01T03:25:54+00:00",
+        "end_time": "2022-05-01T23:26:54+00:00"
+    }))
     const token = useToken().token;
 
-    const fetchData = () => {
+    const fetchData = (deviceSerial) => {
+        if(deviceSerial == undefined){
+            deviceSerial = currentDeviceSerial
+        }
         axios.post(url_getDeviceLog, deviceSerial , {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
             .then((response) => {
-                setdevicelog(response.data.device_log)
+                setDeviceLog(response.data.device_log)
+                console.log(response)
+            });
+    };
+    const fetchDataW = (deviceSerial) => {
+        if(deviceSerial == undefined){
+            deviceSerial = currentDeviceSerial
+        }
+        axios.post(url_getWateringLog, deviceSerial , {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then((response) => {
+                setWateringLog(response.data.wateringLog)
                 console.log(response)
             });
     };
 
     useEffect(() => {
         fetchData()
+        fetchDataW()
     }, []);
 
-    const setDeviceSerial = (deviceSerial, beginTime, endTime) =>{
-        setdeviceserial(deviceSerial, beginTime, endTime)
-        fetchData()
+    const setDeviceSerial = (inputSerial, beginTime, endTime) =>{
+        setCurrentDeviceSerial(JSON.stringify({
+            "device_serial": inputSerial,
+            "begin_time": beginTime + "T00:00:00+00:00",
+            "end_time": endTime + "T23:59:59+00:00",
+        }))
+        fetchData(JSON.stringify({
+            "device_serial": inputSerial,
+            "begin_time": beginTime + "T00:00:00+00:00",
+            "end_time": endTime + "T23:59:59+00:00",
+        }))
+        fetchDataW(JSON.stringify({
+            "device_serial": inputSerial,
+            "begin_time": beginTime + "T00:00:00+00:00",
+            "end_time": endTime + "T23:59:59+00:00",
+        }))
     }
     return (
         <>
@@ -47,11 +82,30 @@ const Report = () => {
             <div className="page-title">
                  گزارش ها
             </div>
-        <ResponsiveContainer width={"90%"} height={500}>
+            <Form type="date" className="date-form">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label> سریال دستگاه</Form.Label>
+                    <FormControl type="text" placeholder="سریال"
+                                 onChange={ (event) => setSerial(event.target.value) }
+                    />
+                    <Form.Label>تاریخ شروع</Form.Label>
+                    <Form.Control type="date" placeholder="تاریخ شروع"
+                                  onChange={ (event) => setBeginDate(event.target.value) }
+                    />
+                    <Form.Label>تاریخ پایان</Form.Label>
+                    <Form.Control type="date" placeholder="تاریخ پایان"
+                                  onChange={ (event) => setEndDate(event.target.value) }
+                    />
+                </Form.Group>
+                    <Button onClick={() => setDeviceSerial(inputSerial, inputBeginDate, inputEndDate)} variant="outline-secondary" id="button-addon2" className="add-device-btn">
+                        مشاهده
+                    </Button>
+            </Form>
+        <ResponsiveContainer className="chartclass" width={"100%"} height={500}>
             <LineChart
                 width={500}
                 height={300}
-                data={devicelogs}
+                data={deviceLogs}
                 margin={{
                     top: 5,
                     right: 30,
@@ -65,7 +119,26 @@ const Report = () => {
                 <Tooltip />
                 <Legend />
                 <Line type="monotone" dataKey="humidity" stroke="#8884d8" activeDot={{ r: 8 }} />
-                {/*<Line type="monotone" dataKey="uv" stroke="#82ca9d" />*/}
+            </LineChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer className="chartclas" width={"100%"} height={500}>
+            <LineChart
+                width={500}
+                height={300}
+                data={wateringLogs}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="time" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="water_amount" stroke="#82ca9d" activeDot={{ r: 8 }} />
             </LineChart>
         </ResponsiveContainer>
             </>
